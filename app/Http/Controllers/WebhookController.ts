742cf586998cdac387132b82498d77a7e0e12b48
João Lenon <lenonsec7@gmail.com>
2021-05-14 15:58:33 -0300

    feat: Initial Commit

--- app/Http/Controllers/WebhookController.ts
+++ app/Http/Controllers/WebhookController.ts
@@ -1,11 +1,23 @@
 import { ApiTags } from '@nestjs/swagger'
-import { Controller, Inject, Post, Query } from '@nestjs/common'
+import { Body, Controller, Inject, Post, Query } from '@nestjs/common'
 import { TelegrafCollection } from 'app/Services/Collections/TelegrafCollection'
 
 @Controller('/webhooks')
 @ApiTags('Webhook')
 export class WebhookController {
   @Inject(TelegrafCollection) private telegrafCollection: TelegrafCollection
+
+  @Post('cloudAmqp')
+  async cloudAmqp(@Body() body: any) {
+    const options = {
+      from: 'CloudAmqp',
+      url: body.hostname,
+      details: JSON.stringify(body.options),
+      name: body.appname,
+    }
+
+    return this.telegrafCollection.sendMessage(options)
+  }
 
   @Post('uptimeRobot')
   async uptimeRobot(@Query() queries: any) {
